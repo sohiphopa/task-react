e34ae3544370854a95aa11684e03c985e013a2bd
sohiphopa <136040617+sohiphopa@users.noreply.github.com>
2024-05-07 00:41:04 +0300

    Initial commit

---
+++ src/compoenents/About.js
@@ -0,0 +1,18 @@
+function About() {
+    return (
+        <div>
+        <h1> About Us page: </h1>
+        <p>
+        
+
+        Brand Story: Share the origin story of your business. Explain why you started it, what inspired you, and what sets you apart from competitors. Make it memorable and personal to connect with your audience
+        </p>
+        <p>Who You Serve: Clearly define what you offer and who your product or service is for. Explain how it solves problems or meets needs for your target audience</p>
+        <p>How You Operate: If you offer a service, explain your business model or how your products are made. Highlight any unique approaches or processes that set you apart</p>
+        <p>Team Introduction: Feature photos and brief bios of the founders or key team members. Customers like to see the faces behind the brand</p>
+        <p>Persuasive Content: Consider what action you want readers to take next. Use additional content, such as testimonials, buttons, explainer videos, or links to blog posts, to move them toward that goal</p>
+        </div>
+    );
+}
+
+export default About;
